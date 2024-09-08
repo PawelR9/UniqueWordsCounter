25350f19a2418cdcf018f8febcb3db8261533af6
PawelR9 <pawelrylski@protonmail.com>
2024-09-08 17:53:46 +0200

    1st commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        ClassLoader classLoader = Main.class.getClassLoader();
+        URL resource = classLoader.getResource("inputFile.txt");
+
+        if (resource == null) {
+            throw new IllegalArgumentException("Plik inputFile.txt nie został znaleziony!");
+        }
+
+        File file = new File(resource.getFile());
+
+        String text = FileUtils.readFileToString(file).toLowerCase().replaceAll("[^a-z0-9\\s]", "");
+        String[] words = text.split(" ");
+
+        Map<String, Integer> countMap = new HashMap<>();
+        for(String word : words) {
+            countMap.put(word, countMap.getOrDefault(word, 0) + 1);
+        }
+
+        StringBuilder outputText = new StringBuilder();
+
+        for (Map.Entry<String, Integer> entry : countMap.entrySet()) {
+            outputText.append(entry.getKey()).append(" ").append(entry.getValue()).append("\n");
+        }
+
+        FileUtils.writeStringToFile(new File("outputFile.txt"),outputText.toString(), StandardCharsets.UTF_8);
+
+    }
+}
